1e36edf285bb9b8d69877225d7954c8fd3c2e85a
zed <zed@mail.ru>
2020-11-01 23:38:34 +0300

    работает но до конца

--- postcss.config.js
+++ postcss.config.js
@@ -6,12 +6,9 @@ const cssnano = require('cssnano');
 
 module.exports = {
     // подключите плагины к PostCSS
-    plugins: [
-        // подключите autoprefixer
-        autoprefixer,
-        // cssnano при подключении нужно передать объект опций
-        // { preset: default } говорит о том, что нужно использовать
-        // стандартные настройки минификации
-        cssnano({ preset: 'default' })
-    ]
+    plugins: {
+        'postcss-preset-env': {
+            browsers: 'last 2 versions',
+        },
+    },
 };
