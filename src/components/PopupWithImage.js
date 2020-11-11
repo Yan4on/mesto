e84fa1bafa11f455526fbe255172289a9b1b57dc
Yanis Shirokikh <yan4on@yandex.ru>
2020-11-11 22:14:44 +0300

    сделал верстку попапа и лайк бокса

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,19 +1,18 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-    constructor(popupSelector) {
-        super(popupSelector);
-    }
+  constructor(popupSelector) {
+    super(popupSelector);
+  }
 
-    // Открываем попап Добавления карточки, дополнительно вставлям ссылку, alt и название
-    open({ name, link }) {
-        super.open();
-        const image = this.popup.querySelector(".popup__img-big");
-
-        const title = this.popup.querySelector(".popup__title");
-
-        image.src = link;
-        image.alt = name;
-        title.textContent = name;
-    }
+  // Открываем попап Добавления карточки, дополнительно вставлям ссылку, alt и название
+  open({name, link}) {
+    super.open();
+    const image = this.popup.querySelector(".popup__img-big");
+    const title = this.popup.querySelector(".popup__title");
+    
+    image.src = link;
+    image.alt = name;
+    title.textContent = name;
+  }
 }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,19 +1,19 @@
 export default class UserInfo {
-    constructor(profileName, profileInterst) {
-        this._profileName = document.querySelector(profileName);
-        this._profileInterst = document.querySelector(profileInterst);
-    }
+  constructor(profileName, profileInterst) {
+    this._profileName = document.querySelector(profileName);
+    this._profileInterst = document.querySelector(profileInterst);
+  }
 
-    //получить данные со страницы
-    getUserInfo() {
-        const name = this._profileName.textContent;
-        const interst = this._profileInterst.textContent;
-        return { name, interst };
-    }
+  //получить данные со страницы
+  getUserInfo() {
+    const name = this._profileName.textContent;
+    const interst = this._profileInterst.textContent;
+    return { name, interst };
+  }
 
-    //установить данные на страницу
-    setUserInfo({ name, interst }) {
-        this._profileName.textContent = name;
-        this._profileInterst.textContent = interst;
-    }
+  //установить данные на страницу
+  setUserInfo({ name, interst }) {
+    this._profileName.textContent = name;
+    this._profileInterst.textContent = interst;
+  }
 }
